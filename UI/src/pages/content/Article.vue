3de35659faa8c6f44458c6ed7cb5e44e09fd2e6d
AntaresBoy <2531646835@qq.com>
2022-02-19 23:40:18 +0800

    addproc

--- UI/src/pages/content/Article.vue
+++ UI/src/pages/content/Article.vue
@@ -1,104 +1,113 @@
 <template>
   <div class="article">
-    <el-row class="el-row" type="flex">
-      <el-col :span="16" class="el-col">
-        <el-input placeholder="标题" :title="title" v-model="title"></el-input>
+    <el-row class="el-row"
+            type="flex">
+      <el-col :span="16"
+              class="el-col">
+        <h1 v-if="isViewType">{{title}}</h1>
+        <el-input v-if="!isViewType"
+                  placeholder="标题"
+                  :title="title"
+                  v-model="title"></el-input>
       </el-col>
-      <el-col :span="8" class="el-col">
-        <el-select
-          clearable
-          v-model="selectedTags"
-          multiple
-          filterable
-          allow-create
-          default-first-option
-          :reserve-keyword="false"
-          placeholder="请选择文章所属类别"
-        >
-          <el-option
-            v-for="item in TAGS"
-            :key="item.value"
-            :label="item.label"
-            :value="item.value"
-          >
+      <el-col :span="8"
+              class="el-col">
+        <el-select v-if="!isViewType"
+                   clearable
+                   v-model="selectedTags"
+                   multiple
+                   filterable
+                   allow-create
+                   default-first-option
+                   :reserve-keyword="false"
+                   placeholder="请选择文章所属类别">
+          <el-option v-for="item in TAGS"
+                     :key="item.value"
+                     :label="item.label"
+                     :value="item.value">
           </el-option>
         </el-select>
       </el-col>
     </el-row>
 
-    <MdEditor ref="mdEditor" thume="github" :onSave="onSave"></MdEditor>
+    <MdEditor ref="mdEditor"
+              thume="github"
+              :onSave="onSave"></MdEditor>
   </div>
 </template>
 
 <script lang="ts">
-import { useRoute } from 'vue-router'
-import { useMessage, useCloser } from '@/hooks/web/useMessage'
+import { useRoute } from "vue-router";
+import { useMessage, useCloser } from "@/hooks/web/useMessage";
 import {
   defineComponent,
   reactive,
   ref,
   defineAsyncComponent,
   onMounted,
-} from 'vue'
-import { getDetailById, updateBlogById, newBlogApi } from '@/api/blog/blog'
-import { isSuccess } from '@/utils/http/index'
-import { UpdateBlogType, NewBlogType } from '@/api/model/blogsModel'
-import { TAGS } from '@/config/const/const'
-import { useOpenLoading,useCloseLoading } from '@/hooks/common/useLoading';
+} from "vue";
+import { getDetailById, updateBlogById, newBlogApi } from "@/api/blog/blog";
+import { isSuccess } from "@/utils/http/index";
+import { UpdateBlogType, NewBlogType } from "@/api/model/blogsModel";
+import { TAGS } from "@/config/const/const";
+import { useOpenLoading, useCloseLoading } from "@/hooks/common/useLoading";
 
 export default defineComponent({
-  name: 'Article',
+  name: "Article",
   components: {
-    MdEditor: defineAsyncComponent(() =>
-      import('@/components/md-editor/MdEditor.vue')
+    MdEditor: defineAsyncComponent(
+      () => import("@/components/md-editor/MdEditor.vue")
     ),
   },
   setup(props, context) {
-    const { params } = useRoute()
-    const mdEditor: any = ref(null)
-    let title = ref('')
-    let selectedTags = ref<string[]>([])
+    const { params } = useRoute();
+    const isViewType = ref<boolean>(false);
+    const mdEditor: any = ref(null);
+    let title = ref("");
+    let selectedTags = ref<string[]>([]);
 
     async function newBlog() {
       const data: NewBlogType = {
         tags: selectedTags.value,
         title: title.value,
         content: mdEditor.value.content,
-      }
-      const result = await newBlogApi(data)
+      };
+      const result = await newBlogApi(data);
       if (isSuccess(result)) {
-        useCloser('创建')
+        useCloser("创建");
       } else {
-        useMessage('创建失败!', 'error')
+        useMessage("创建失败!", "error");
       }
     }
 
     async function getBlogDetail(contentId: string) {
-      const instance=useOpenLoading(true)
-      const result: any = await getDetailById(contentId)
-      const type = params.type
+      const instance = useOpenLoading(true);
+      const result: any = await getDetailById(contentId);
+      const type = params.type;
       if (isSuccess(result)) {
         setTimeout(() => {
-          if (mdEditor.value && type === 'edit') {
-            mdEditor.value.mode = 'editable'
+          if (mdEditor.value && type === "edit") {
+            mdEditor.value.mode = "editable";
           } else {
-            mdEditor.value.mode = 'preview'
+            mdEditor.value.mode = "preview";
           }
-          mdEditor.value.content = result.data.data.content
-        }, 100)
-        title.value = result.data.data.title
-        selectedTags.value = result.data.data.tags.split(',')
+          mdEditor.value.content = result.data.data.content;
+        }, 100);
+        title.value = result.data.data.title;
+        selectedTags.value = result.data.data.tags.split(",");
       } else {
-        useMessage('获取文章失败！', 'error')
+        useMessage("获取文章失败！", "error");
       }
-       useCloseLoading(instance)
+      useCloseLoading(instance);
     }
 
     onMounted(async () => {
-      if ((params.contentId as string) !== 'newBlog') {
-        getBlogDetail(params.contentId as string)
+      console.log("111", params.type);
+      isViewType.value = params.type === "view" ? true : false;
+      if ((params.contentId as string) !== "newBlog") {
+        getBlogDetail(params.contentId as string);
       }
-    })
+    });
 
     async function updateBlog(contentId: string) {
       const data: UpdateBlogType = {
@@ -106,20 +115,20 @@ export default defineComponent({
         title: title.value,
         contentId,
         tags: selectedTags.value,
-      }
-      const result: any = await updateBlogById(data)
+      };
+      const result: any = await updateBlogById(data);
       if (isSuccess(result)) {
-        useCloser('更新')
+        useCloser("更新");
       } else {
-        useMessage('博客更新失败！', 'error')
+        useMessage("博客更新失败！", "error");
       }
     }
 
     async function onSave() {
-      if ((params.contentId as string) !== 'newBlog') {
-        updateBlog(params.contentId as string)
+      if ((params.contentId as string) !== "newBlog") {
+        updateBlog(params.contentId as string);
       } else {
-        newBlog()
+        newBlog();
       }
     }
 
@@ -129,9 +138,10 @@ export default defineComponent({
       onSave,
       TAGS,
       selectedTags,
-    }
+      isViewType,
+    };
   },
-})
+});
 </script>
 
 <style lang="less" scoped>
@@ -148,5 +158,11 @@ button {
 }
 .article {
   margin: 5px 5px;
+}
+h1 {
+  margin-top: 10px;
+  text-align: center;
+  font-weight: 600;
+  font-size: 2.2rem;
 }
 </style>
